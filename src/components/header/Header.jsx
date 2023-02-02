9fa15d9324c01c7890f067408e1aa856ed1c7cdc
rensilva5 <renzoasilva@gmail.com>
2023-02-02 14:19:37 -0500

    Header component content and styled

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import Cta from './Cta'
+import mephoto from '../../assets/mephoto.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I am</h5>
+        <h1>Renzo Silva</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <Cta />
+        <HeaderSocials/>
+        <div className="me">
+          <img src={mephoto} alt='' />
+        </div>
+        <a href='#contact' className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
+
   )
 }
 
