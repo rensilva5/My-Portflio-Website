f4ea6e8ea0ba4f728f577320f5c2daf9f881836d
rensilva5 <renzoasilva@gmail.com>
2023-02-02 00:45:57 -0500

    Creating components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+// rafce => to create a station functional component
+import React from 'react'
+import About from './components/about/About'
+import Contacts from './components/contacts/Contacts'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contacts />
+      <Footer />
+    </>
+  )
+}
+
+export default App
