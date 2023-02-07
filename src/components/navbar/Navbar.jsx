6acb229ed5edf03a680e1a974d35a09af61d011d
rensilva5 <renzoasilva@gmail.com>
2023-02-07 00:12:52 -0500

    NavBar component with floting style, and fixed position on the screen

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { icons } from 'react-icons'
 import './navbar.css'
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai"
@@ -8,13 +8,14 @@ import { FaCode } from "react-icons/fa"
 import { BiMessageSquareDetail } from "react-icons/bi"
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href='#' className='active'><AiOutlineHome/></a>
-      <a href='#about'><AiOutlineUser/></a>
-      <a href='#experience'><BiBook/></a>
-      <a href='#portfolio'><FaCode/></a>
-      <a href='#contact'><BiMessageSquareDetail/></a>
+      <a href='#' onClick={() => setActiveNav('#')} className={ activeNav === '#' ? 'active' : '' }><AiOutlineHome/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={ activeNav === '#about' ? 'active' : '' }><AiOutlineUser/></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={ activeNav === '#experience' ? 'active' : ''} ><BiBook/></a>
+      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={ activeNav === '#portfolio' ? 'active' : '' }><FaCode/></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={ activeNav === '#contact' ? 'active' : '' }><BiMessageSquareDetail/></a>
     </nav>
   )
 }
