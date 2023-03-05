b43e4950b66637f5edad8327ccce8c7c7d9e59fb
rensilva5 <renzoasilva@gmail.com>
2023-03-05 00:52:10 -0500

    New changes on portflio pages

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,7 +12,7 @@ const data = [
     id: 1,
     image: img1,
     title: "My Personal Portfolio",
-    description: "Personal project",
+    description: "As a personal portfolio website, it showcases my professional work, achievements, and skills. It serves as an online portfolio that demonstrates my expertise and capabilities in my field of work display in several sections about me, my work experience, education, and skills.",
     github: "https://github.com/rensilva5/My-Portflio-Website",
     // demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboard-and-finantial-data-visualication'
   },
@@ -20,7 +20,7 @@ const data = [
     id: 2,
     image: img2,
     title: "Europe Planner",
-    description: "Personal project",
+    description: "A Europe trip planner is a practical tool that helps travelers plan their itinerary for a trip to Europe. It typically includes features such as destination recommendations.",
     github: "https://github.com/rensilva5/front-end-your-o-planner",
     demo: "https://your-o-planner.web.app/",
   },
@@ -28,7 +28,7 @@ const data = [
     id: 3,
     image: img3,
     title: "Blog-Website",
-    description: "Personal project",
+    description: "My blog website hosts regularly updated posts or articles in a chronological order. These posts are typically written by an individual or a group of authors on various topics of interest.",
     github: "https://github.com/rensilva5/blog-website",
     // demo: 'https://dribbble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
   },
@@ -36,7 +36,7 @@ const data = [
     id: 4,
     image: img7,
     title: "Personal Todo App",
-    description: "Personal project",
+    description: "My personal to-do app has been designed to help individuals manage their tasks, goals, and responsibilities. I provide a user-friendly interface that allows you to create a list of tasks or projects, set due dates, and track your progress towards completion.",
     github: "https://github.com/rensilva5/three-do-web",
     demo: "https://three-do-rs.web.app/",
   },
@@ -44,7 +44,7 @@ const data = [
     id: 5,
     image: img5,
     title: "Patient List",
-    description: "Personal project",
+    description: "The Patient List app's primary goal is to help healthcare professionals manage their patient information efficiently. I provide a user-friendly interface that allows users to create and view patient profiles, manage patient data, and keep track of appointments and medical histories.",
     github: "https://github.com/rensilva5/create-patients-list-app",
     // demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboard-and-finantial-data-visualication'
   },
@@ -52,7 +52,7 @@ const data = [
     id: 6,
     image: img6,
     title: "Ticket Tracker",
-    description: "Personal project",
+    description: "Ticket Tracker app helps teams manage their tasks and projects efficiently, manage ticket data, and keep track of project progress. Teams can create tickets that include relevant information such as title, description, priority level, deadline, and any other relevant notes.",
     github: "https://github.com/rensilva5/Hackathon-2022",
     // demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboard-and-finantial-data-visualication'
   },
